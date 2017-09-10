5b8ba0ef00e6cbecbed08ecd2ff72681a69fe6e6
Viranch Mehta <email@viranch.me>
2017-09-10 23:46:56 +0530

    Add convenience script to update letsencrypt certs

---
+++ vps/certs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -ue
+set -x
+
+DOMAIN=$1
+shift
+
+docker stop nginx
+
+sudo letsencrypt certonly --standalone -d $DOMAIN $@
+
+LIVE_PATH=/etc/letsencrypt/live/$DOMAIN
+CERT_FILE=/opt/vps/ssl_certs/$DOMAIN
+
+sudo cat $LIVE_PATH/fullchain.pem > $CERT_FILE.crt
+sudo cat $LIVE_PATH/privkey.pem > $CERT_FILE.key
+
+test -f $CERT_FILE.dhparam.pem || openssl dhparam -out $CERT_FILE.dhparam.pem 2048
+
+docker start nginx
