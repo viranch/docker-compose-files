bb8525e124db9361ba3241b6c7e001bcf9a57e22
Viranch Mehta <email@viranch.me>
2019-03-11 21:42:27 -0700

    VPS- fix Icinga notify script

--- vps/icinga/scripts/notify.sh
+++ vps/icinga/scripts/notify.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-curl https://api.pushbullet.com/v2/pushes -H 'Content-Type: application/json' -H "Access-Token: $2" -d @- << EOF
-{
-  "title": "Icinga",
-  "body": "$1"
-  "type": "note"
-}
-EOF
+# curl is not available in icinga docker image
+# use check_http instead
+
+/usr/lib/nagios/plugins/check_http -S --sni -H api.pushbullet.com -u /v2/pushes -k 'Content-Type: application/json' -k "Access-Token: $2" -P '{"title":"Icinga","body":"'"$1"'","type":"note"}'
