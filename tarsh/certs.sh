c7309c9d66c0c2d96b8664b5476ef07bc0cd32ef
Viranch Mehta <email@viranch.me>
2021-06-10 21:32:25 -0700

    add new server

---
+++ tarsh/certs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+##
+# Usage:
+#   ./certs.sh ./nginx/certs foo.com www.foo.com,api.foo.com
+##
+
+set -x
+
+CERT_PATH=$1
+DOMAIN=$2
+test -n "$3" && DOMAINS=$DOMAIN,$3 || DOMAINS=$DOMAIN
+
+docker stop nginx
+
+sudo certbot certonly --standalone -d $DOMAINS
+
+LIVE_PATH=/etc/letsencrypt/live/$DOMAIN
+CERT_FILE=$CERT_PATH/$DOMAIN
+
+sudo cat $LIVE_PATH/fullchain.pem > $CERT_FILE.crt
+sudo cat $LIVE_PATH/privkey.pem > $CERT_FILE.key
+
+test -f $CERT_FILE.dhparam.pem || openssl dhparam -out $CERT_FILE.dhparam.pem 2048
+
+docker start nginx
